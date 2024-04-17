86a06189996f02c8fe398bddadbcffd97c3c4f25
Alok Sharma <56442060+alok744@users.noreply.github.com>
2024-04-18 01:40:18 +0530

    Student_class

---
+++ Student.cs
@@ -0,0 +1,10 @@
+﻿namespace webapi_core_crud_operation
+{
+    public class Student
+    {
+        public int studentid { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string city { get; set; }
+    }
+}
